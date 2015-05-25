f367a8e551cd986e132ab2e41e2fbc0b2139f075
Jonathan Pool <reddevpool@gmail.com>
2015-05-25 16:08:21 -0700

    Completed Game Tutorial

---
+++ GalacticAttack/Assets/Scrips/Explosion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	void Start()
+	{
+		Invoke ("Die", 1f);
+	}
+
+	void Die()
+	{
+		Destroy (gameObject);
+
+	}
+}
